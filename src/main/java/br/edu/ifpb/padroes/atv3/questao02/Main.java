2e36ebca5c74b5c722a5135fafe1b859bff753b5
Gabriel Silva <gabrieldasilvamonteiro2@gmail.com>
2025-08-24 00:10:01 -0300

    Questao 2 da atividade implementada

--- src/main/java/br/edu/ifpb/padroes/atv3/questao02/Main.java
+++ src/main/java/br/edu/ifpb/padroes/atv3/questao02/Main.java
@@ -10,35 +10,35 @@ import br.edu.ifpb.padroes.atv3.questao02.item.Item;
 public class Main {
 
     public static void main(String[] args) {
-        // Itens simples
+        //Criando itens mais simples, prato, bebida e sobremesas
         Item pizza = new Prato("Pizza", 30.0,"Pizza de calabresa com catupiry");
         Item refri = new Bebida("Refrigerante", 8.0,"Refrigerante de 750ml");
         Item sobremesa = new Sobremesa("Pudim", 12.0,"Pudim de chocolate");
 
         System.out.println("=============================================");
 
-        // Combo 1
+        //Criando combos juntando prato, bebidas, sobremesas
         Combo comboAlmoco = new Combo("Almoço Executivo", 5.0,"Novo combo de almoço",0);
         comboAlmoco.adicionar(pizza);
         comboAlmoco.adicionar(refri);
         comboAlmoco.adicionar(sobremesa);
 
 
-        // Combo 2 que contém outro combo
+        //Criando combos juntando prato, bebidas, sobremesas e também outros combos
         Combo superCombo = new Combo("Super Combo", 10.0,"Novo super combo",0);
         superCombo.adicionar(comboAlmoco);
         superCombo.adicionar(new Prato("Batata Frita", 15.0,"Batata frita com queijo"));
 
 
 
-        // Cardápio do restaurante
+        //Criando e adicionando itens ao cardápio do restaurante
         Cardapio cardapio = new Cardapio();
         cardapio.adicionar(pizza);
         cardapio.adicionar(refri);
         cardapio.adicionar(comboAlmoco);
         cardapio.adicionar(superCombo);
 
-        // Exibir tudo
+        // Exibindo todos os itens do cardápio do restaurante
         cardapio.exibir();
     }
 }
